1bfd11a1307dfaa32175b42a9dd38281427a2117
IT21305214 <IT21305214@my.sliit.lk>
2026-04-25 14:53:03 +0530

    fix mongodb connection

--- server.js
+++ server.js
@@ -4,15 +4,21 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
+
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/easylodge')
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://sandaligeethmadias_db_user:Sands0528@cluster0.wrijfks.mongodb.net/easylodge?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('DB Error:', err));
 
-// Routes
 app.use('/api/boarders', require('./routes/boarders'));
 
 const PORT = process.env.PORT || 5000;
